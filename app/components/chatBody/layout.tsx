18e288d9d110bf57119b30bbf1b2ac09d144a8fe
Вадим Бекмансуров <vadimbekmansurov@Vadims-MacBook-Pro.local>
2024-08-20 22:42:22 +0500

    added main files for root layout

---
+++ app/components/chatBody/layout.tsx
@@ -0,0 +1,11 @@
+import style from './style.module.scss'
+
+function ChatBody() {
+	return (
+		<div className={style.root}>
+			
+		</div>
+	)
+}
+
+export default ChatBody
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ChatBody from './components/chatBody/layout';
 
 export default function Home() {
   return (
     <div className={styles.root}>
-      
+      <ChatBody />
     </div>
   );
 }
